cb33e3ea0f4d3449690c40f40e3185aeab44624f
Estrella <gracielabenitesf@gmail.com>
2020-05-05 13:55:06 -0500

    changing wpp message

--- index_files/app.js
+++ index_files/app.js
@@ -1,11 +1,11 @@
 $('#wpp-msg').on('click', function(){
 	window.open("https://api.whatsapp.com/send?phone=51971313111&text=Quiero%20hablar%20con%20una%20funcionaria");
 })
-/*
+
 $('#text-box').hide();
 
 $('#wpp_welcome_wrapper').hover(function() {
-	console.log('hover');
-	$('#text-box').toggle();
+  $('#text-box').toggle();
 })
-*/
+
+
