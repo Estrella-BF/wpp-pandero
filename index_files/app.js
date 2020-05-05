02229e7afc05f0be13d414fd8734f26725e6c5a0
Estrella <gracielabenitesf@gmail.com>
2020-05-05 12:59:12 -0500

    adding styles

--- index_files/app.js
+++ index_files/app.js
@@ -1,11 +1,11 @@
 $('#wpp-msg').on('click', function(){
 	window.open("https://api.whatsapp.com/send?phone=51971313111&text=I'm%20interested%20in%20your%20portfolio");
 })
-
+/*
 $('#text-box').hide();
 
-$('#wpp').hover(function() {
+$('#wpp_welcome_wrapper').hover(function() {
 	console.log('hover');
 	$('#text-box').toggle();
 })
-
+*/
